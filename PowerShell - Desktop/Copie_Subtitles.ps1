9171e96fb92e7ba06b9b21ab67a81dee05938155
monneryf <monnery.fabien@gmail.com>
2019-11-08 18:36:55 +0100

    2019-11-08

---
+++ PowerShell - Desktop/Copie_Subtitles.ps1
@@ -0,0 +1,9 @@
+﻿# Déclaration des variables
+$Path = 'E:\Videos'
+$Destination = 'C:\Users\monne\Desktop\Subtitles'
+
+#Copy-Item -Filter *.txt -Path $Path -destination $Destination
+
+set-location $Path
+Get-ChildItem *.srt -Recurse | Copy-Item -Destination $Destination
+
